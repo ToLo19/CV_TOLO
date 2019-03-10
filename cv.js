35c3ca828fd437d8c6cdee450bb7983a7301dd42
ToLo <m507hp@gmail.com>
2019-03-11 00:42:15 +0400

    'stage_2)))'

--- cv.js
+++ cv.js
@@ -20,7 +20,6 @@ class User {
             ul.appendChild(li);
         })
         template.appendChild(ul);
-        
     }
 }
 
@@ -35,4 +34,32 @@ let createNewUser = () => {
     } else {
         alert('Заполнте все поля!');
     }
+}
+
+// Создает еще один Input 
+let createMoreLanguages = () => {
+    let languageDiv = document.getElementById('languageDiv');
+    let create = document.createElement('input');
+    create.setAttribute('class', 'language');
+    create.setAttribute('placeholder', 'Language(s)')
+    languageDiv.appendChild(create);
+}
+
+// Создает несколько связанных input-ов
+let createMoreWork = () => {
+    let workDiv = document.getElementById('workDiv');
+    let create = document.createElement('input');
+    let create2 = document.createElement('input');
+    let create3 = document.createElement('input');
+    let div = document.createElement('div');
+    create.setAttribute('class', 'work');
+    create2.setAttribute('class', 'since');
+    create3.setAttribute('class', 'untill');
+    create.setAttribute('placeholder', 'Work(s) at')
+    create2.setAttribute('placeholder', 'Since')
+    create3.setAttribute('placeholder', 'Untill')
+    workDiv.appendChild(create);
+    workDiv.appendChild(div);
+    div.appendChild(create2);
+    div.appendChild(create3);
 }
